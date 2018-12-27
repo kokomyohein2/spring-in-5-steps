4a994edbc15eaffb8424bf4684949b3e0de5e9d5
Ko-PN <komyo2000@gmail.com>
2018-12-27 16:37:08 +0700

    ep39 added @postconstruct and @predestroy

--- src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -5,6 +5,11 @@
  */
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -18,6 +23,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("bubble")
@@ -33,5 +40,15 @@ public class BinarySearchImpl {
         System.out.println(sortAlgorithm);
 
         return 3;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestory(){
+        logger.info("postConstruc");
     }
 }
